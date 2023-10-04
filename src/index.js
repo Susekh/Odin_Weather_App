43394f5b65b39511b4197942f1cc957488e60323
Subh <subhranshukhilar@outlook.com>
2023-10-05 00:21:42 +0530

    Added the contents inside a box

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
 import './style.css';
 
+const container = document.getElementById('container');
+const contentContainer = document.createElement('div');
+contentContainer.classList.add('form-div');
 const formDiv = document.createElement('form');
+formDiv.classList.add('form-cont');
 
 function takeInput() {
-  const container = document.getElementById('container');
-
   formDiv.innerHTML = `
-    <label for="location">Enter the Location to get weather</label>
-    <input id='location' type="text">
+    <label for="location">Enter the Location to get weather</label><br><br>
+    <input id='location' type="text" required><br><br>
     <input type="submit">
   `;
-
-  container.appendChild(formDiv);
+  contentContainer.appendChild(formDiv);
+  container.appendChild(contentContainer);
 }
 takeInput();
 
 // fetch the weather api and console.log it
+function renderDiv(weatherData) {
+  const getLocation = document.getElementById('location').value;
+  const showTempDiv = document.createElement('div');
+  showTempDiv.classList.add('show-temp-div');
+
+  showTempDiv.innerText = `Your current weather in ${getLocation} is ${weatherData.current.temp_c}`;
+  contentContainer.appendChild(showTempDiv);
+}
 
 async function getWeather() {
   const getLocation = document.getElementById('location').value;
-
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=10d8920911ff496591871009231009&q=${getLocation}`, { mode: 'cors' });
-  const weatherData = await response.json();
-  console.log(weatherData.current.temp_c);
+  try {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=10d8920911ff496591871009231009&q=${getLocation}`, { mode: 'cors' });
+    const weatherData = await response.json();
+    renderDiv(weatherData);
+  } catch (err) {
+    // eslint-disable-next-line no-alert
+    alert('Enter a valid place');
+  }
 }
 
 formDiv.addEventListener('submit', (e) => {
